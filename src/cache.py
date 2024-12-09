1e8b7a1a7305c6a4243c1a4ce0b33edf2f62fc30
Mohamed-Ousalem YAHIAOUI <130100153+RogerD22@users.noreply.github.com>
2024-12-09 16:11:31 +0100

    Update cache.py

--- src/cache.py
+++ src/cache.py
@@ -12,6 +12,5 @@ def sauvegarder_cache_hachages(cache_file, cache_hachages):
     try:
         with open(cache_file, "w") as f:
             json.dump(cache_hachages, f, indent=4)
-        print(f"Cache sauvegardé dans {cache_file}")
     except Exception as e:
         print(f"Erreur lors de la sauvegarde du cache : {e}")
