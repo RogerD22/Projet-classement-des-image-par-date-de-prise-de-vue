556d79a71254cfb74dae41e512fe5aa4414f5f61
Mohamed-Ousalem YAHIAOUI <130100153+RogerD22@users.noreply.github.com>
2024-12-09 15:58:52 +0100

    Update organisateur_photos.py
    Ajout du help

--- src/organisateur_photos.py
+++ src/organisateur_photos.py
@@ -2,31 +2,36 @@ import utils
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Organise les photos selon leurs métadonnées EXIF.")
-    parser.add_argument("source", type=str, help="Chemin du dossier source contenant les photos.")
+    parser = argparse.ArgumentParser(
+        description="Organise les photos selon leurs métadonnées EXIF, permettant de les trier dans des dossiers basés sur la date de prise de vue. "
+                    "Vous pouvez choisir d'organiser les photos par année, mois, ou année et mois. "
+                    "Pour plus d'informations sur les tokens `strftime`, veuillez consulter la documentation officielle : https://docs.python.org/3/library/datetime.html")
+
+    
+    parser.add_argument("source", type=str, help="Chemin du dossier source contenant les photos à organiser.")
+
     parser.add_argument("destination", type=str, help="Chemin du dossier destination où organiser les photos.")
     
-    # Rendre l'argument 'format' optionnel
-    parser.add_argument("format", type=str, nargs="?", help="Format d'organisation des photos (utilisez des tokens strftime). Exemple : '%Y/%m/%d' pour année/mois/jour.")
+    parser.add_argument("format", type=str, nargs="?", help="Format d'organisation des photos (utilisez des tokens strftime). Exemple : '%%Y/%%m/%%d' pour année/mois/jour. "
+             "Si non spécifié, vous pouvez choisir entre l'option mois, année ou année/mois. "
+             "Pour plus d'informations sur les tokens strftime, consultez la documentation officielle : https://docs.python.org/3/library/datetime.html")
     
-    # Groupe pour l'option exclusive de tri
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument("-m", "--mois", action="store_true", help="Organiser les photos par mois.")
-    group.add_argument("-a", "--annee", action="store_true", help="Organiser les photos par année.")
-    group.add_argument("-am", "--annee_mois", action="store_true", help="Organiser les photos par année et mois.")
+    group.add_argument("-m", "--mois", action="store_true", help="Organiser les photos par mois, avec le format '%%B'.")
+    group.add_argument("-a", "--annee", action="store_true", help="Organiser les photos par année, avec le format '%%Y'.")
+    group.add_argument("-am", "--annee_mois", action="store_true", help="Organiser les photos par année et mois, avec le format '%%Y/%%B'.")
     
     args = parser.parse_args()
 
-    
     if args.format:
         format_organisation = args.format  
     elif args.mois:
-        format_organisation = "%m"  
+        format_organisation = "%B"  
     elif args.annee:
         format_organisation = "%Y"  
     elif args.annee_mois:
-        format_organisation = "%Y/%m"
-   
+        format_organisation = "%Y/%B"
+    else:
+        format_organisation = "%Y/%B"
 
-    # Lancer l'organisation avec le format spécifié par l'utilisateur
     utils.organiser_photos(args.source, args.destination, format_organisation)
